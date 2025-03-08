a439df577d4e28a31ccc89107c04422e9585bb99
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-08 23:25:34 +0200

    feat(managers): add direction management to allow horizontal and vertical ship placement

---
+++ src/managers/direction-manager.ts
@@ -0,0 +1,22 @@
+import { Direction } from "../types";
+
+const horizontalBtn: HTMLButtonElement | null = document.querySelector("#horizontalBtn");
+const verticalBtn: HTMLButtonElement | null = document.querySelector("#verticalBtn");
+
+let currentDirection: Direction = "right";
+
+horizontalBtn?.addEventListener("click", () => {
+    currentDirection = "right";
+    horizontalBtn.classList.add("active");
+    verticalBtn?.classList.remove("active");
+});
+
+verticalBtn?.addEventListener("click", () => {
+    currentDirection = "bottom";
+    verticalBtn.classList.add("active");
+    horizontalBtn?.classList.remove("active");
+});
+
+export default function getDirection() {
+    return currentDirection;
+}

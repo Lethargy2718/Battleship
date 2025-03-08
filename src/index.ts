30274c684441d05db743328e1e0579a30afd9c17
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-08 23:30:05 +0200

    refactor(grid): relocate grid drawing logic to a separate module

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,5 @@
-import './style.css';
+import "./style.css";
+import "./managers/drag-manager";
 
-const shipMap = {
-    battleship: 5,
-    submarine: 4,
-    destroyer: 3,
-    patrol: 2,
-}
-
-function drawGrid() {
-    const grid = document.querySelector(".game-board");
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            const cell = document.createElement("div");
-            cell.setAttribute("data-x", `${i}`);
-            cell.setAttribute("data-y", `${j}`);
-            cell.classList.add("cell");
-
-            grid?.appendChild(cell);
-        }
-    }
-}
-
+import drawGrid from "./utils/create-menu-grid";
 drawGrid();

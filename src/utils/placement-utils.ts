b9806514bab5a10a2de6a05d74a7524490a0b960
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-08 23:18:39 +0200

    feat(utils): add ship placement validity checker

---
+++ src/utils/placement-utils.ts
@@ -0,0 +1,35 @@
+import { Coordinate, Vector } from "../types/direction";
+
+export function checkPlacement(coord: Coordinate, shipLength: number, vector: Vector, grid: string[][]) {
+    const cells = new Map<Coordinate, boolean>();
+    const { dx, dy } = vector;
+    let isValid: boolean = true;
+
+    for (let i = 0; i < shipLength; i++) {
+        const x = coord.x + i * dx;
+        const y = coord.y + i * dy;
+
+        if (x >= 10 || y >= 10) {
+            return { isValid: false, cells: cells };
+        }
+
+        if (grid[x][y]) {
+            isValid = false;
+            cells.set({ x, y }, false);
+        }
+
+        cells.set({ x, y }, true);
+    }
+
+    return { isValid, cells };
+}
+
+export function handlePlacement(coord: Coordinate, shipLength: number, vector: Vector, grid: string[][]) {
+    const results = checkPlacement(coord, shipLength, vector, grid);
+    console.log(results.cells);
+    if (results.isValid) {
+        return true;
+    } else {
+        return false;
+    }
+}

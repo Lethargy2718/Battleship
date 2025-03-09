df385455a03497115613f33f156eeaab2545055b
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-09 23:34:47 +0200

    feat(utils): add a 2d matrix of the HTML cells

--- src/utils/create-menu-grid.ts
+++ src/utils/create-menu-grid.ts
@@ -1,13 +1,18 @@
 export default function drawGrid() {
     const grid = document.querySelector(".game-board");
+    const gridMatrix: HTMLDivElement[][] = [];
     for (let i = 0; i < 10; i++) {
+        gridMatrix.push([]);
         for (let j = 0; j < 10; j++) {
             const cell = document.createElement("div");
             cell.setAttribute("data-x", `${i}`);
             cell.setAttribute("data-y", `${j}`);
             cell.classList.add("cell");
+            gridMatrix[i].push(cell);
 
             grid?.appendChild(cell);
         }
     }
+
+    return gridMatrix;
 }

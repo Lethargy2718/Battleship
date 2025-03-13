9fea124531a8411a294b58d94fc56428ca92f0b0
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-13 16:38:46 +0200

    refactor(utils): move fillBoard and remove param
    - Move fillBoard function to place-ship.ts.
    - Remove redundant parameter from placeShip function.

--- src/utils/place-ship.ts
+++ src/utils/place-ship.ts
@@ -1,8 +1,15 @@
-import { Coordinate, Direction, Ship } from "../types";
+import { Coordinate, Direction, Ship, ShipPlacement } from "../types";
 import createShip from "./create-ship";
 
 // This function is only called when placement is valid
-export function placeShip(startingCell: Coordinate, direction: Direction, ship: Ship, img: HTMLImageElement, cells: Coordinate[], gameBoard: HTMLDivElement): void {
+export function placeShip(startingCell: Coordinate, direction: Direction, ship: Ship, img: HTMLImageElement, gameBoard: HTMLDivElement): void {
     const shipDiv = createShip(startingCell, ship, img, direction);
     gameBoard?.appendChild(shipDiv);
+}
+
+export function fillBoard(shipPlacementArr: ShipPlacement[], shipToImg: Record<Ship, HTMLImageElement>, gameBoard: HTMLDivElement): void {
+    for (const { startingCell, direction, ship } of shipPlacementArr) {
+        const img = shipToImg[ship];
+        placeShip(startingCell, direction, ship, img, gameBoard);
+    }
 }

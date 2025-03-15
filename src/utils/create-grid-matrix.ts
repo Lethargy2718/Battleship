63f31c631d46063cbde820cb87a22d66e70251cb
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-15 06:27:14 +0200

    refactor(utils): add generic type to grid matrices
    - Add generic type parameter to allow for flexible matrix types.

--- src/utils/create-grid-matrix.ts
+++ src/utils/create-grid-matrix.ts
@@ -1,5 +1,5 @@
 import { CellState } from "../types";
 
-export default function createGridMatrix(size = 10, defaultValue: CellState = ""): CellState[][] {
+export default function createGridMatrix<T = CellState>(size = 10, defaultValue: T = "" as T): T[][] {
     return new Array(size).fill(null).map(() => new Array(size).fill(defaultValue));
 }
